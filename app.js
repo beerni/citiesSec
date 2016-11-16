2ce7c0658bdcede67ec8a46a18cd70ca3f9905c8
Bernat Mir <bernatmir13@gmail.com>
2016-11-16 13:01:09 +0100

    Skeleton

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http        = require('http');
+
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public/app')));
+
+var routes = require('./routes');
+app.use('/', routes);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.set('port',process.env.PORT || 8080);
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
