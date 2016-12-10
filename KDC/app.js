943d6e3321d9c4d872070a754662c02cb1698784
Bernat Mir <bernatmir13@gmail.com>
2016-12-10 15:55:55 +0100

    KDC added and certificates ssl

---
+++ KDC/app.js
@@ -0,0 +1,56 @@
+/**
+ * Created by bernatmir on 10/12/16.
+ */
+/**
+ * Created by bernatmir on 4/10/16.
+ */
+var express = require('express');
+var path = require('path');
+//var favicon = require('serve-favicon');
+var logger = require('morgan');
+var routes = require('./routes/index');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var https = require('https');
+
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use('/', routes);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+var options = {
+    key: fs.readFileSync('kdc-key.pem'),
+    cert: fs.readFileSync('kdc-cert.pem')
+};
+
+
+https.createServer(options, app).listen(8085, function () {
+    console.log('Started!');
+});
+module.exports = app;
+
+/*var server = require('http').Server(app);
+
+ // Start server
+ server.listen(8085, function() {
+ console.log("TTP running on http://localhost:8085");
+ });*/
---
+++ KDC/routes/index.js
@@ -0,0 +1,60 @@
+/**
+ * Created by bernatmir on 4/10/16.
+ */
+var express = require('express');
+var router = express.Router();
+var CryptoJS = require("crypto-js");
+var rsa = require('./rsa');
+var keys = rsa.generateKeys(512);
+var bignum = require('bignum');
+var request = require('request');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+router.post('/ttp', function (req, res) {
+    console.log('from Alice');
+    console.log(req.body);
+    compareHash(req.body.data, function () {
+        var A='Alice';
+        var B = 'Bob';
+        var K=req.body.data.K;
+        var TTP='TTP';
+        var concat=TTP+'|'+A+'|'+B+'|'+K;
+        var concatHash = CryptoJS.SHA256(concat);
+        concatHash= bignum(concatHash.toString(),16);
+        var proofOfPublicationK = concatHash.powm(keys.privateKey.d, keys.publicKey.n);
+        var data={
+            TTP:TTP,
+            A:A,
+            B:B,
+            K:K,
+            proofPublication:proofOfPublicationK.toString(),
+            eTTP:keys.publicKey.e.toString(),
+            nTTP:keys.publicKey.n.toString()
+        };
+        res.status(200).send({data:data});
+        request({
+            uri: "https://localhost:8080/publicationProof",
+            method: "POST",
+            form: data
+        }, function(error, response, body) {
+            console.log(error);
+        });
+    });
+   function compareHash(info, cb) {
+       var concat = info.A + '|' + info.TTP + '|' + info.B +'|' + info.K;
+       var eC = bignum(info.eClient);
+       var nC= bignum(info.nClient);
+       var originHash = CryptoJS.SHA256(concat).toString();
+       var originServer = bignum(info.originProofOfK);
+       var decrypted = originServer.powm(eC,nC).toString(16);
+       if(decrypted.localeCompare(originHash)==0){
+           console.log('Equal hashes from proof of K!');
+           console.log(decrypted.toString(16));
+           console.log(originHash.toString(16));
+           cb();
+       }
+       else
+           console.log('mismatch');
+   }
+});
+
+module.exports = router;
