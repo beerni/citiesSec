1b69d3a9dc2d4c4278a50f096063db4862b8e86a
Aitor Piñeyro <itor95@gmail.com>
2017-01-04 17:30:54 +0100

    add img and some improves

--- models/ad.js
+++ models/ad.js
@@ -6,25 +6,28 @@ var mongoose = require('mongoose');
 Schema = mongoose.Schema;
 
 var adSchema = new Schema({
-    _id:{
-        type: String
+        _id: {
+            type: String
+        },
+        title: {
+            type: String
+        },
+        price: {
+            type: String
+        },
+        description: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        imgurl: {
+            type: String
+        }
     },
-    title: {
-        type: String
-    },
-    price: {
-        type: String
-    },
-    description: {
-        type: String
-    },
-    username: {
-        type: String
-    }
-},
-{
-    _id: false,
-    versionKey: false // You should be aware of the outcome after set to false (elimina __V)
-});
+    {
+        _id: false,
+        versionKey: false // You should be aware of the outcome after set to false (elimina __V)
+    });
 
 module.exports = mongoose.model('Ad', adSchema);
--- routes/ad.js
+++ routes/ad.js
@@ -2,19 +2,23 @@
  * Created by aitor on 4/1/17.
  */
 var express = require('express');
-var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs-extra');
+var crypto = require('crypto');
 var Ad = require('../models/ad.js');
-const crypto = require("crypto");
-
+var router = express.Router();
+var base_url = "https://localhost:8080";
 
 //Add an ad
 router.post('/add', function (req, res) {
+    if (!req.body.imgurl) req.body.imgurl = base_url + 'img/ad-img/default_img.png';
     var ad = new Ad({
         _id: crypto.randomBytes(16).toString("hex"),
         title: req.body.title,
         price: req.body.price,
         description: req.body.description,
-        username: req.body.username
+        username: req.body.username,
+        imgurl: req.body.imgurl
     });
 
     ad.save(function (err, a) {
@@ -22,6 +26,34 @@ router.post('/add', function (req, res) {
         res.status(200).json(a);
     });
 });
+
+//Add image
+router.post('/update/:id', function (req, res) {
+    var form = new formidable.IncomingForm();
+    var target_path;
+    var filname = crypto.randomBytes(16).toString("hex");
+    form.parse(req, function (err, fields, files) {
+        var tmp_path = files.file.path; // file is the name html input that contain us route img
+        var tipo = files.file.type; // type file
+
+        if (tipo == 'image/png' || tipo == 'image/jpg' || tipo == 'image/jpeg') {
+            target_path = './public/web/img/ad-img/' + filname + ".png";// route to add us file and concatenate filename
+            fs.rename(tmp_path, target_path, function (callback) {
+                fs.unlink(tmp_path, function (callback) {});
+            });
+        }
+    });
+
+    Ad.findById(req.params.id, function (err, ad) {
+        ad.imgurl = base_url + '/web/img/ad-img/' + filname;
+
+        ad.save(function (err, a) {
+            if (err) return res.send(500, err.message);
+            res.status(200).json(a);
+        });
+    })
+});
+
 
 //Get all ads
 router.get('/getAll', function (req, res) {
