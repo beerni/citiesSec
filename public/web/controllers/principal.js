ced5e480f4e2747b919ef4c859ce1e485b4ce9f9
Bernat Mir <bernatmir13@gmail.com>
2017-01-07 20:01:41 +0100

    Logout added

--- public/web/controllers/principal.js
+++ public/web/controllers/principal.js
@@ -1,14 +1,14 @@
 /**
  * Created by bernatmir on 10/12/16.
  */
-angular.module('cities').controller('PrincipalController', ['$http', '$scope','$rootScope', function ($http, $scope,$rootScope) {
+angular.module('cities').controller('PrincipalController', ['$http', '$scope','$rootScope','$window','$cookies', function ($http, $scope,$rootScope,$window,$cookies) {
 
     console.log("index");
 
-
     $scope.logout = function () {
-        console.log("logout");
+        $rootScope.isLogged = false;
+        $rootScope.token = null;
+        $cookies.remove('tokenData');
+        $window.location.href = "https://localhost:8080";
     }
-
-
 }]);
